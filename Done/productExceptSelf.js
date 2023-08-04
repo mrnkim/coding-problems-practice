fca603b008792646041c60c6dcafd3c1d529cfa0
MKim <mrn.kim07@gmail.com>
2023-08-03 22:04:26 -0700

    productExceptSelf - first trial

---
+++ Done/productExceptSelf.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Goal: return an array that has the products of nums (except for the num at the
+ * index) at the input array
+ *
+ *  ! Don't use division operation
+ *  ! Must be O(n) runtime
+ *
+ * Case 1
+ *  [1,2,3,4] => [24, 12, 8, 6]
+ *
+ * Case 2
+ *  [-1, 1, 0, -3, 3] => [0, 0, 9, 0, 0]
+ *
+ * Approach:
+ * 
+ *
+ *
+ *
+ */
