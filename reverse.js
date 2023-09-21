093694d5930e070efec29831360eda14889d877f
MKim <mrn.kim07@gmail.com>
2023-09-21 20:04:40 +0900

    Practice reverse

---
+++ reverse.js
@@ -0,0 +1,32 @@
+/**
+ * Goal: reverses the given string
+ *
+ * Input: string
+ * Output: string (reversed)
+ *
+ * Case 1:
+ * "Hi My name is Andrei" => "ierdnA si eman yM iH"
+ *
+ * Case 2:
+ * "" => ""
+ *
+ * Approach:
+ * Loop over a string backwards
+ * Create an array and put each character from input string to the array
+ * Convert the array to string
+ */
+
+function reverse(string) {
+  if (string.length < 1) {
+    return string;
+  }
+
+  const resultArr = [];
+  for (i = string.length - 1; i >= 0; i--) {
+    resultArr.push(string[i]);
+  }
+
+  return resultArr.join("");
+}
+
+module.exports = reverse;
---
+++ spec/reverse.test.js
@@ -0,0 +1,11 @@
+const reverse = require("../reverse");
+
+describe("reverse", function () {
+  it("is a function", function () {
+    expect(typeof reverse).toEqual("function");
+  });
+
+  it("reverses 'Hi My name is Andrei'", function () {
+    expect(reverse("Hi My name is Andrei")).toEqual("ierdnA si eman yM iH");
+  });
+});
