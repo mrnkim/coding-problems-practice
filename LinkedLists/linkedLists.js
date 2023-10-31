384860fbd56c4afa2f2c41ac1e6ca03a7ba0ba81
MKim <mrn.kim07@gmail.com>
2023-10-30 21:56:02 -0700

    Implemented prepend, printList in linkedLists.js

--- LinkedLists/linkedLists.js
+++ LinkedLists/linkedLists.js
@@ -1,5 +1,10 @@
 "use strict";
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -10,22 +15,56 @@ class LinkedList {
     this.length = 1;
   }
 
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
+
   append(value) {
-    const newTail = {
-      value: value,
-      next: null,
-    };
+    const newTail = new Node(value);
+
     this.tail.next = newTail;
     this.tail = newTail;
     this.length += 1;
 
     return this;
   }
+
+  prepend(value) {
+    const newHead = new Node(value);
+
+    newHead.next = this.head;
+    this.head = newHead;
+    this.length += 1;
+
+    return this;
+  }
+
+  insert(index, value) {
+    const newNode = new Node(value);
+    let currNode = this.head;
+    let nextNode = currNode.next;
+
+    while (i <= index) {
+      currNodeIndex = 0;
+      currNode.next = 
+    }
+
+  }
 }
 
+// console
 const myLinkedList = new LinkedList(10);
 
 console.log(myLinkedList.append(6));
 console.log(myLinkedList.append(3));
 console.log(myLinkedList.append(2));
-// console.log(myLinkedList);
+console.log(myLinkedList.prepend(1));
+console.log(myLinkedList.printList());
