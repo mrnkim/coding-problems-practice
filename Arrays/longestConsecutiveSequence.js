abad2d24d2aa070ec04765fe776a51bdd4e649f5
MKim <mrn.kim07@gmail.com>
2023-11-08 21:01:05 -0800

    Start longestConsecutiveSequence

--- Arrays/longestConsecutiveSequence.js
+++ Arrays/longestConsecutiveSequence.js
@@ -3,9 +3,24 @@
  * Goal: Return the number of the longest consecutive sequence in an array
  *
  * Input: array of numbers
- * Output: number (longest consecutive sequence)
+ * Output: number of the longest consecutive sequence
  *
- * 
+ * ! O(n) time
+ *
+ * Case 1:
+ *  [100,4,200,1,3,2] -> 4
+ * 4 100 200 1 3 2
+ * 4 100 1 200 3 2
+ * 4 100 1 3 200 2
+ * 4 100 1 3 2 200
+ 
+ *
+ * Case 2:
+ *  [0,3,7,2,5,8,4,6,0,1] -> 9
+ *
+ * Approach:
+ *
+ *
  *
  *
  *
