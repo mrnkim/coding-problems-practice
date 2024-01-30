e929937a0e8d5f605514b1bffc76eaf90066463f
MKim <mrn.kim07@gmail.com>
2024-01-30 20:07:36 +0900

    Set up BinarySearchTree

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {}
+
+  lookup(value) {}
+
+  //remove
+}
